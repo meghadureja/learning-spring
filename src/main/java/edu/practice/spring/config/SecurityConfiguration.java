ac5a6566ba918f5a8094b5b302354b660056f73a
Megha Dureja <megha.dureja01@gmail.com>
2020-12-24 09:58:46 +0000

    add h2 database configuration

---
+++ src/main/java/edu/practice/spring/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package edu.practice.spring.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring security blocks /h2-console (or the path you configured in your application.yaml) path for H2 database.
+ *
+ * To access the H2 console just add the below code to your WebSecurityConfigurerAdapter.
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2-console/**").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+}
